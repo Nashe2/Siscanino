62e735536f62decd59b8158cb295fd9fa0555ad8
luisenricke <luisevm24@outlook.com>
2019-11-24 20:03:44 -0600

    fix: actualizacion de las tablas

--- app/src/main/java/com/nashe/siscanino/data/DatabaseRoom.java
+++ app/src/main/java/com/nashe/siscanino/data/DatabaseRoom.java
@@ -12,25 +12,38 @@ import androidx.sqlite.db.SupportSQLiteDatabase;
 import com.nashe.siscanino.data.converter.DateConverter;
 
 
+import com.nashe.siscanino.data.dao.ActividadDAO;
 import com.nashe.siscanino.data.dao.AlimentacionDAO;
+import com.nashe.siscanino.data.dao.BañoDAO;
 import com.nashe.siscanino.data.dao.CaninoDao;
 import com.nashe.siscanino.data.dao.CartillaDao;
 import com.nashe.siscanino.data.dao.DesparacitacionDao;
+import com.nashe.siscanino.data.dao.MedicamentoDAO;
+import com.nashe.siscanino.data.dao.PetActividadDAO;
+import com.nashe.siscanino.data.dao.PetAlimentacionDAO;
 import com.nashe.siscanino.data.dao.PetDao;
+import com.nashe.siscanino.data.dao.PetSintomaDAO;
 import com.nashe.siscanino.data.dao.RazaDao;
+import com.nashe.siscanino.data.dao.SintomaDAO;
 import com.nashe.siscanino.data.dao.TipoUsuarioDao;
 import com.nashe.siscanino.data.dao.UserDao;
 import com.nashe.siscanino.data.dao.UserPetDao;
 import com.nashe.siscanino.data.dao.UsuarioCaninoJoinDao;
 import com.nashe.siscanino.data.dao.UsuarioDao;
 import com.nashe.siscanino.data.dao.VacunaDao;
+import com.nashe.siscanino.data.entity.Actividad;
 import com.nashe.siscanino.data.entity.Alimentacion;
+import com.nashe.siscanino.data.entity.Baño;
 import com.nashe.siscanino.data.entity.Canino;
 import com.nashe.siscanino.data.entity.Cartilla;
 import com.nashe.siscanino.data.entity.Desparacitacion;
+import com.nashe.siscanino.data.entity.Medicamento;
 import com.nashe.siscanino.data.entity.Pet;
+import com.nashe.siscanino.data.entity.PetActividad;
 import com.nashe.siscanino.data.entity.PetAlimentacion;
+import com.nashe.siscanino.data.entity.PetSintoma;
 import com.nashe.siscanino.data.entity.Raza;
+import com.nashe.siscanino.data.entity.Sintoma;
 import com.nashe.siscanino.data.entity.TipoUsuario;
 import com.nashe.siscanino.data.entity.User;
 import com.nashe.siscanino.data.entity.UserPet;
@@ -40,7 +53,6 @@ import com.nashe.siscanino.data.entity.Vacuna;
 
 @SuppressWarnings("SpellCheckingInspection")
 @Database(entities = {
-
         User.class,
         Pet.class,
         UserPet.class,
@@ -49,12 +61,17 @@ import com.nashe.siscanino.data.entity.Vacuna;
         Desparacitacion.class,
         Alimentacion.class,
         PetAlimentacion.class,
-        // Nuevas tablas
         TipoUsuario.class,
         Usuario.class,
         UsuarioCaninoJoin.class,
         Canino.class,
-        Raza.class
+        Raza.class,
+        Actividad.class,
+        Baño.class,
+        Sintoma.class,
+        PetActividad.class,
+        PetSintoma.class,
+        Medicamento.class
 },
         version = 1, exportSchema = false)
 @TypeConverters({DateConverter.class})
@@ -86,6 +103,21 @@ public abstract class DatabaseRoom extends RoomDatabase {
     public abstract CaninoDao caninoDAO();
 
     public abstract RazaDao razaDAO();
+
+    public abstract PetAlimentacionDAO petAlimentacionDAO();
+
+    public abstract ActividadDAO actividadDAO();
+
+    public abstract BañoDAO bañoDAO();
+
+    public abstract SintomaDAO sintomaDAO();
+
+    public abstract PetActividadDAO petActividadDAO();
+
+    public abstract PetSintomaDAO petSintomaDAO();
+
+    public abstract MedicamentoDAO medicamentoDAO();
+
 
     public static DatabaseRoom getInstance(Context context) {
         if (INSTANCE == null) {
